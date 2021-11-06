9e8d2103907d6caa0819e9980ba08311806c9202
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-11-06 14:38:38 -0400

    We mark the skills

--- public/js/app.js
+++ public/js/app.js
@@ -2065,6 +2065,21 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+//
+//
+//
 //
 //
 //
@@ -2075,8 +2090,27 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   props: ['skills'],
-  mounted: function mounted() {
-    console.log(this.skills);
+  mounted: function mounted() {//console.log(this.skills)
+  },
+  data: function data() {
+    return {
+      skillss: new Set()
+    };
+  },
+  methods: {
+    active: function active(e) {
+      if (e.target.classList.contains('bg-green-500')) {
+        e.target.classList.remove('bg-green-500');
+        this.skillss["delete"](e.target.textContent);
+      } else {
+        e.target.classList.add('bg-green-500');
+        this.skillss.add(e.target.textContent);
+      }
+
+      var stringSkills = _toConsumableArray(this.skillss);
+
+      document.querySelector('#skills').value = stringSkills;
+    }
   }
 });
 
@@ -37634,18 +37668,30 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "ul",
-    { staticClass: "flex flex-wrap justify-center" },
-    _vm._l(this.skills, function(skill, i) {
-      return _c(
-        "li",
-        { key: i, staticClass: "border border-gray-500 p-3 m-2 rounded" },
-        [_vm._v(_vm._s(skill) + "\n    ")]
-      )
-    }),
-    0
-  )
+  return _c("div", [
+    _c(
+      "ul",
+      { staticClass: "flex flex-wrap justify-center" },
+      _vm._l(this.skills, function(skill, i) {
+        return _c(
+          "li",
+          {
+            key: i,
+            staticClass: "border border-gray-500 p-3 m-2 rounded",
+            on: {
+              click: function($event) {
+                return _vm.active($event)
+              }
+            }
+          },
+          [_vm._v(_vm._s(skill))]
+        )
+      }),
+      0
+    ),
+    _vm._v(" "),
+    _c("input", { attrs: { type: "hidden", id: "skills", name: "skills" } })
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/components/SkillsList.vue
+++ resources/js/components/SkillsList.vue
@@ -1,16 +1,37 @@
 <template>
-    <ul class="flex flex-wrap justify-center">
-        <li class="border border-gray-500 p-3 m-2 rounded"
-            v-for="(skill, i) in this.skills"
-            v-bind:key="i">{{ skill }}
-        </li>
-    </ul>
+    <div>
+        <ul class="flex flex-wrap justify-center">
+            <li class="border border-gray-500 p-3 m-2 rounded"
+                v-for="(skill, i) in this.skills"
+                v-bind:key="i"
+                @click="active($event)">{{ skill }}</li>
+        </ul>
+        <input type="hidden" id="skills" name="skills">
+    </div>
 </template>
 <script>
 export default {
     props: ['skills'],
     mounted() {
-        console.log(this.skills)
+        //console.log(this.skills)
+    },
+    data: function () {
+        return {
+            skillss: new Set()
+        }
+    },
+    methods: {
+        active(e) {
+            if (e.target.classList.contains('bg-green-500')) {
+                e.target.classList.remove('bg-green-500')
+                this.skillss.delete(e.target.textContent)
+            } else {
+                e.target.classList.add('bg-green-500')
+                this.skillss.add(e.target.textContent)
+            }
+            const stringSkills = [...this.skillss];
+            document.querySelector('#skills').value = stringSkills;
+        }
     }
 }
 </script>
