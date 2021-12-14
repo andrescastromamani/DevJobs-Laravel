32582d5da62fa58a3984708c71deb925d6a9a935
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-12-14 14:45:59 -0400

    We add latest vacancies in the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,19 +19,22 @@
                 </div>
             </div>
             <div class="mt-10 p-10 shadow bg-white rounded-lg">
-                <h2 class="text-black-50 text-center">Nuevas Vacantes</h2>
-                <ul>
+                <h1 class="text-black-50 text-center font-bold">Nuevas Vacantes</h1>
+                <ul class="grid grid-cols-1 md:grid-cols-2 gap-8">
                     @foreach($vacancies as $vacancy)
-                        <li class="flex items-center mt-5">
-                            <div class="w-1/3 mx-5">
+                        <li class="flex items-center mt-5 border rounded-lg shadow p-3">
+                            <div class="w-1/5">
                                 <img class="w-20"
                                      src="/storage/vacancies/{{$vacancy->image}}" alt="">
                             </div>
-                            <div class="w-2/3">
+                            <div class="w-4/5">
                                 <h3 class="text-xl font-bold">{{$vacancy->title}}</h3>
                                 <p class="text-gray-500">{!! $vacancy->category->name !!}</p>
+                                <p class="text-gray-500">Ubicacion: {!! $vacancy->location->title !!}</p>
+                                <p class="text-gray-500">Experiencia: {!! $vacancy->experience->title !!}</p>
+                                <p class="text-gray-500">Salario: {!! $vacancy->salary->title !!}</p>
                                 <a href="{{route('vacancies.show', $vacancy->id)}}"
-                                   class="text-green-500">Ver Vacante</a>
+                                   class="text-green-500 font-bold">Ver Vacante</a>
                             </div>
                         </li>
                     @endforeach
